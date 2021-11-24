8627c1cb9860292bbf066a5d4ae8abc37c756d90
Jacub <38807049+JacubRSTNC@users.noreply.github.com>
2021-11-24 18:07:46 +0100

    yes

---
+++ src/components/NavBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="navbar">
+    <h2 id="navbartext">{{ siteName }}</h2>
+  </div>
+</template>
+<script>
+export default {
+  name: "NavBar",
+  props: {
+    siteName: String,
+  },
+};
+</script>
+<style scoped>
+#navbar {
+  background-color: #333;
+  color: #fff;
+  top: 0;
+  left: 0;
+  position: absolute;
+  width: 100%;
+}
+#navbartext {
+  margin-left: 1vw;
+  font-size: 1.5em;
+}
+</style>
