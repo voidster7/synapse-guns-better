1f5b79aca4f7e821f7c4521d8eb82645c3d5ca7d
Jacub <38807049+JacubRSTNC@users.noreply.github.com>
2021-12-16 19:00:27 +0100

    Remove hardcoded items

--- src/store/index.js
+++ src/store/index.js
@@ -2,43 +2,7 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    items: {
-      "kriss_vector": {
-        name: "Kriss Vector",
-        image: "img/weapon_cw_vector.png",
-        legal: true,
-        marketprice: 1000,
-        itemType: "assaultrifle",
-      },
-      "thompson": {
-        name: "Thompson",
-        image: "img/weapon_cw_thompson.png",
-        legal: true,
-        marketprice: 750,
-        itemType: "assaultrifle",
-      },
-      "ak47": {
-        name: "AK-47",
-        image: "img/weapon_cw_ak74.png",
-        legal: false,
-        marketprice: 5600,
-        itemType: "assaultrifle",
-      },
-      "mr96": {
-        name: "MR96",
-        image: "img/weapon_cw_357.png",
-        legal: true,
-        marketprice: 3000,
-        itemType: "pistol",
-      },
-      "ksg12": {
-        name: "KSG-12",
-        image: "img/weapon_cw_tr09_ksg12.png",
-        legal: false,
-        marketprice: 3000,
-        itemType: "shotgun",
-      },
-    },
+    items: {},
     itemsToCraft: {}
   },
   mutations: {
