8e7314222714b73e4c6a5f1ae0c8769fd6075c2b
Jacub <38807049+JacubRSTNC@users.noreply.github.com>
2021-12-21 23:36:32 +0100

    Change formatting & make options load first

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
-  <iframe v-if="shouldFlatbedBeVisible" width="560" height="315" id="flatbed" src="https://www.youtube.com/embed/aYYWW7V25Ok?rel=0&controls=0&showinfo=0&autoplay=1&mute=1" allow="autoplay; encrypted-media"></iframe>
+  <iframe
+    v-if="shouldFlatbedBeVisible"
+    width="560"
+    height="315"
+    id="flatbed"
+    src="https://www.youtube.com/embed/aYYWW7V25Ok?rel=0&controls=0&showinfo=0&autoplay=1&mute=1"
+    allow="autoplay; encrypted-media"
+  ></iframe>
   <NavBar siteName="MonoGuns Revamped"></NavBar>
   <Menu v-if="isMenuOpen"></Menu>
   <div id="parent">
@@ -9,7 +16,9 @@
   <footer v-if="shouldFooterBeVisible">
     <p>Website is in early development.</p>
     <p style="text-align: center">#addtheflatbed</p>
-    <p class="smallText" style="text-align: center">Don't wanna see this? Turn it off in the settings</p>
+    <p class="smallText" style="text-align: center">
+      Don't wanna see this? Turn it off in the settings
+    </p>
   </footer>
 </template>
 
@@ -29,17 +38,17 @@ export default {
   },
   methods: {
     async fetchItems() {
-      let items = await axios.get("./items.json");
-      let materials = await axios.get("./materials.json");
-      this.$store.commit("setItems", items.data);
-      this.$store.commit("setMaterials", materials.data);
-      let options = JSON.parse(localStorage.getItem("options")) || {}
+      let options = JSON.parse(localStorage.getItem("options")) || {};
       for (let i in options) {
         this.$store.commit("setOption", {
           option: i,
           value: options[i],
         });
       }
+      let items = await axios.get("./items.json");
+      let materials = await axios.get("./materials.json");
+      this.$store.commit("setItems", items.data);
+      this.$store.commit("setMaterials", materials.data);
     },
   },
   computed: {
@@ -51,7 +60,7 @@ export default {
     },
     shouldFooterBeVisible() {
       return this.$store.getters.getOption("footer");
-    }
+    },
   },
 };
 </script>
