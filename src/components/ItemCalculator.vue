23372c08b627564051a04f38935126df350033e3
Jacub <38807049+JacubRSTNC@users.noreply.github.com>
2022-01-02 19:50:38 +0100

    Fix issue with recursion

--- src/components/ItemCalculator.vue
+++ src/components/ItemCalculator.vue
@@ -154,6 +154,8 @@ export default {
             } else {
               complete[material.identifier] = material;
             }
+          } else if(material.materials) {
+            shouldRecurse = true;
           }
         } else {
           if (material.materials) {
