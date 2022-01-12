aa6897e0da4b08d69ba879731870f400144b7337
Jacub <38807049+JacubRSTNC@users.noreply.github.com>
2022-01-12 19:12:29 +0100

    include items with no materials to processed items list

--- src/components/ItemCalculator.vue
+++ src/components/ItemCalculator.vue
@@ -212,7 +212,7 @@ export default {
         // loop through all the materials of the step
         let material = stepMaterials[mat];
         if (this.$store.getters.getOption("processedMats")) {
-          if (material.type == "orebar" || material.type == "plank") {
+          if (material.type == "orebar" || material.type == "plank" || !material.materials) {
             if (complete[material.identifier]) {
               complete[material.identifier].amount += material.amount;
             } else {
